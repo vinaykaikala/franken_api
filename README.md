d502f9c49b78d8740fe955d94869f3484f927953
vinaykaikala <vinay.kaikala.ki@gmail.com>
2020-02-22 23:23:21 +0000

    db migration added

--- README.md
+++ README.md
@@ -14,3 +14,21 @@ Franken Plot  API
 ```
     - flanken_api -p 8090
 ```
+
+- Run DB Migration script:
+
+```
+sudo docker exec -it probiouidocker_postgress_1 bash
+su postgres
+psql
+CREATE USER referral_writer WITH ENCRYPTED PASSWORD '<password: this should match with password in flask app settings.py>';
+CREATE DATABASE referrals;
+CREATE DATABASE curation;
+GRANT ALL PRIVILEGES ON DATABASE referrals TO referral_writer;
+GRANT ALL PRIVILEGES ON DATABASE curation TO referral_writer;
+exit
+
+python flanken_api/migrate.py db init --multidb
+python flanken_api/migrate.py db migrate
+python flanken_api/migrate.py db upgrade
+```
---
+++ flanken_api/migrate.py
@@ -0,0 +1,44 @@
+import logging.config
+import click
+from flask import Flask, Blueprint
+
+from flanken_api import settings
+from flanken_api.api.flanken.endpoints.flanken_api import ns as flanken_namespace
+from flanken_api.api.flanken.endpoints.referral_db_api import ns2 as referral_namespace
+from flanken_api.api.flanken.endpoints.curation_db_api import ns3 as curation_namespace
+from flanken_api.api.restplus import api
+from flanken_api.database import db
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+def configure_app(flask_app):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
+    flask_app.config['SQLALCHEMY_BINDS'] = {'curation': settings.SQLALCHEMY_BINDS}
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
+    flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
+    flask_app.config['RESTPLUS_VALIDATE'] = settings.RESTPLUS_VALIDATE
+    flask_app.config['RESTPLUS_MASK_SWAGGER'] = settings.RESTPLUS_MASK_SWAGGER
+    flask_app.config['ERROR_404_HELP'] = settings.RESTPLUS_ERROR_404_HELP
+    flask_app.config['PROBIO'] = settings.MOUNT_POINT_PROBIO
+    flask_app.config['PSFF'] = settings.MOUNT_POINT_PSFF
+
+def initialize_app(flask_app):
+    configure_app(flask_app)
+    blueprint = Blueprint('api', __name__, url_prefix='/api')
+    api.init_app(blueprint)
+    api.add_namespace(flanken_namespace)
+    api.add_namespace(referral_namespace)
+    api.add_namespace(curation_namespace)
+    flask_app.register_blueprint(blueprint)
+    db.init_app(flask_app)
+
+
+app = Flask(__name__)
+initialize_app(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
